7abd59ed7d68df0e000cace02bc7dacbdb47b0a3
Otávio Henrique <ohmcruz07@gmail.com>
2024-08-07 18:39:16 +0000

    Finalização da versão  mobile da página sobre nós.

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,3 +1,4 @@
+
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
@@ -24,29 +25,33 @@ function AboutUs() {
 
         <div className="flex flex-col gap-4 mb-5">
           <h2 className="text-2xl font-header text-h1Color text-center">Nossos valores</h2>
-          <div className="mb-5 flex">
+          <div className="mb-5 flex gap-4">
             <img src={CheckB} alt="" />
             <div>
               <h3 className="text-xl text-h1Color font-semibold">Acessibilidade</h3>
               <p className=" text-textBody">Garantir que todos os produtos e serviços sejam totalmente acessíveis.</p>
             </div>
           </div>
-          <div className="mb-5 flex">
+          <div className="mb-5 flex gap-4">
             <img src={CheckB} alt="" />
             <div>
               <h3 className="text-xl text-h1Color font-semibold">Empatia</h3>
               <p className=" text-textBody">Ouvir e entender as necessidades reais dos usuários para oferecer soluções verdadeiramente eficazes.</p>
             </div>
           </div>
-          <div className="mb-5">
+          <div className="mb-5 flex gap-4">
             <img src={CheckB} alt="" />
+            <div>
             <h3 className="text-xl text-h1Color font-semibold">Inovação</h3>
             <p className=" text-textBody">Buscar continuamente novas maneiras de melhorar e evoluir as soluções.</p>
+            </div>
           </div>
-          <div className="mb-5">
+          <div className="mb-5 flex gap-4">
             <img src={CheckB} alt="" />
+            <div>
             <h3 className="text-xl text-h1Color font-semibold">Autonomia</h3>
             <p className=" text-textBody">Promover a independência de nossos usuários em suas rotinas de saúde.</p>
+            </div>
           </div>
           <img src={ValueImg} alt="" className="mb-5"/>
         </div>
@@ -55,30 +60,30 @@ function AboutUs() {
           <h2 className="text-2xl font-header text-h1Color text-center">Nosso time</h2>
           <p className="text-center text-textBody">Com uma equipe dedicada de desenvolvedores, designers e especialistas em saúde, estamos comprometidos em ouvir, entender e atender às necessidades dos usuários. Cada funcionalidade do nosso aplicativo foi desenvolvida com base em feedback real, garantindo que seja intuitiva, acessível e, acima de tudo, útil.</p>
         </div>
-        <div className="flex flex-wrap gap-4 mb-7">
+        <div className="flex flex-wrap justify-center gap-2 mb-12">
           <img src={Mateus} alt="Mateus" />
           <img src={Gustavo} alt="Gustavo" />
           <img src={Caua} alt="Cauã" />
           <img src={Otavio} alt="Otávio" />
         </div>
 
-        <div>
-        <h1 className="text-2xl font-header text-white text-center">Fale Conosco</h1>
+        <div className="bg-gradient-to-b from-blue-500 to-cyan-500 pt-8 pb-10 px-8 mb-28">
+        <h1 className="text-2xl font-header text-white text-center mb-2">Fale Conosco</h1>
         <form>
-            <div>
-                <label htmlFor="name">Nome</label>
-                <input type="text" name="Nome" id="name" placeholder="Digite seu nome" required/>
+            <div className="flex flex-col mb-5">
+                <label htmlFor="name" className="text-white">Nome</label>
+                <input className="rounded-md placeholder:text-h1Color py-1.5 pl-0.5" type="text" name="Nome" id="name" placeholder="Digite seu nome" required/>
             </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Digite seu email" required/>
+            <div className="flex flex-col mb-5">
+                <label htmlFor="email" className="text-white">Email</label>
+                <input className="rounded-md placeholder:text-h1Color py-1.5 pl-0.5" type="email" id="email" name="email" placeholder="Digite seu email" required/>
             </div>
-            <div>
-                <label htmlFor="feedback">Feedback</label>
-                <textarea id="feedback" name="feedback" placeholder="Digite seu feedback" required></textarea>
+            <div className="flex flex-col mb-10">
+                <label htmlFor="feedback" className="text-white">Feedback</label>
+                <textarea className="rounded-md placeholder:text-h1Color pl-0.5 pt-1.5 pb-28" id="feedback" name="feedback" placeholder="Digite seu feedback" required></textarea>
             </div>
-            <div>
-                <button type="submit">Enviar</button>
+            <div className="flex justify-center text-center">
+                <button type="submit" className="py-3 px-24 bg-linerColor2 rounded-md text-white">Enviar</button>
             </div>
         </form>
         </div>
